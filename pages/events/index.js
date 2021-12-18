312a0e4057db2a976427f21c3136ff6eed8c6cb7
Prakash-Reddy08 <mjprakashreddy1@gmail.com>
2021-12-18 14:06:00 +0530

    change eventPage,create temp Event api

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,29 @@
 import Layout from '../../components/Layout'
+import Eventitem from "../../components/Eventitem";
+import { API_URL } from "../../config"
 
-export default function EventPage() {
+export default function EventsPage({ events }) {
     return (
         <Layout>
-            My Event
+            <h1>Events</h1>
+            {events.length === 0 && <h3>No events to show</h3>}
+            {
+                events.map(evt => {
+                    return (<Eventitem key={evt.id} evt={evt} />)
+                })
+            }
         </Layout>
     )
+}
+
+export const getStaticProps = async () => {
+    const res = await fetch(`${API_URL}/api/events`)
+    const events = await res.json();
+
+    return {
+        props: {
+            events
+        },
+        revalidate: 60
+    }
 }
